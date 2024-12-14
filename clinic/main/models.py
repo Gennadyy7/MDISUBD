c50b06f9186270d16f23a97d4f65ff838070df62
Gennady_Hvesko <pprom5573@gmail.com>
2024-12-14 23:04:00 +0300

    Поправил отображение пользователя

--- clinic/main/models.py
+++ clinic/main/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
-        return f'{self.last_name}'
+        return f'{self.username} - {self.last_name} {self.first_name} {self.patronymic}'
 
     class Meta:
         verbose_name = 'Пользователь'
